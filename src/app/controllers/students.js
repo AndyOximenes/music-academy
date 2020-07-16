641d7402e9215f135c4bb6b786b9d09159629751
AndyOximenes <andy.oximenes@gmail.com>
2020-07-16 16:28:59 -0300

    Preparing project to receive postgres db

---
+++ src/app/controllers/students.js
@@ -0,0 +1,45 @@
+const { date } = require("../../lib/utils");
+
+module.exports = {
+    index(request, response) {
+        return response.render("students/index");
+    },
+
+    post(request, response) {
+        const keys = Object.keys(request.body);
+
+        for (key of keys) {
+            if (request.body[key] == "") {
+                return response.send("Please fill all the fields");
+            }
+        }
+        return;
+    },
+
+    create(request, response) {
+        return response.render("students/create");
+    },
+
+    show(request, response) {
+        return;
+    },
+
+    edit(request, response) {
+        return;
+    },
+
+    put(request, response) {
+        const keys = Object.keys(request.body);
+
+        for (key of keys) {
+            if (request.body[key] == "") {
+                return response.send("Please fill all the fields");
+            }
+        }
+        return;
+    },
+
+    delete(request, response) {
+        return;
+    },
+};
